15680114a3b52ef8c0b88696db6630a9110159ec
tavira <10252356+tavira@users.noreply.github.com>
2023-02-22 10:09:45 +0300

    Step 8: implement brain progression game

---
+++ brain_games/progression_game.py
@@ -0,0 +1,38 @@
+import random
+
+
+def get_game_description():
+    return 'What number is missing in the progression?'
+
+
+def get_progression(progression_length):
+    progression_start_number = random.randint(0, 100)
+    progression_difference = random.randint(1, 10)
+    progression = []
+    for i in range(0, progression_length):
+        progression.append(
+            progression_start_number + i * progression_difference
+        )
+    return progression
+
+
+def get_random_hidden_index(progression_length):
+    return random.randint(0, progression_length - 1)
+
+
+def make_question(progression, hidden_index):
+    return ' '.join(str(x) for x in progression[:hidden_index]) + \
+        ' .. ' + ' '.join(str(x) for x in progression[hidden_index + 1:])
+
+
+def get_question_answer_pair():
+    progression_length = 10
+    progression = get_progression(progression_length)
+    hidden_index = get_random_hidden_index(progression_length)
+    question = make_question(progression, hidden_index)
+    answer = str(progression[hidden_index])
+    return (question, answer)
+
+
+if __name__ == '__main__':
+    print(get_question_answer_pair())
